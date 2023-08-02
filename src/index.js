c0cc40a6dadd2fb95bbb75eb0f1d59e94c2f5650
4harry <4harry.hippo@gmail.com>
2023-08-02 14:18:41 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+// const reverse = (n) => {
+    let i = 0;
+    let res = '';
+    n = String(n);
+    while (i < n.length) {
+        res = `${n[i]}${res}`;
+        i++;
+    }
+    return parseInt(res);
 }
+
+// console.log(reverse(-321));
